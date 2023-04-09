e334a952f5977caf3ff9c26c573a39da2e316b12
SaraTamer <sasooelbihery@gmail.com>
2023-04-09 20:05:39 +0200

    GUI done

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
+# date: 9/4/2023
+# internship: Python internship with SyncIntern's
+# Author: Sara Tamer Bihery
+# App title: Alarm clock with GUI
+
+
 import datetime
-from playsound import playsound
+import pygame
+from tkinter import *
+from pygame import mixer
 
 
-def get_time():
-    time = input("Please enter the alarm time in the hh:mm format\n")
-    time_list = time.split(':')
-    return time_list
+def alarming():
+    hr = hour.get()
+    minutes = minute.get()
 
+    dif_hours = int(hr) - datetime.datetime.now().hour
+    dif_min = int(minutes) - datetime.datetime.now().minute
 
-def alarming(time):
+    rings_hour = f"rings in: {dif_hours} hours"
+    rings_min = f" and {dif_min} minutes"
+
+    rings_in.config(text=rings_hour + rings_min, font=("Arial", 15))
+
     while True:
-        if int(time[0]) == datetime.datetime.now().hour and int(time[1]) == datetime.datetime.now().minute:
-            print("Alarming..")
-            playsound("ringtone-126505.mp3")
+        if int(hr) == datetime.datetime.now().hour and int(minutes) == datetime.datetime.now().minute:
+
+            ringing = Label(alarm_app, text="Alarming...", font=("Arial", 30), fg="green")
+            ringing.place(x=300, y=340)
+            file = "C:\\Users\\Apex\\PycharmProjects\\AlarmClock\\ringtone-126505-[AudioTrimmer.com].mp3"
+            pygame.mixer.Sound(file).play()
             break
 
 
-alarming(get_time())
+alarm_app = Tk()
+mixer.init()
+
+alarm_app.title("Alarm app")
+
+alarm_app.geometry('750x400')
+
+time_required = Label(alarm_app, text="Please enter the alarm time", height=2, font=("Arial", 20))
+time_required.pack()
+
+hour = StringVar()
+hour.set('00')
+hour_input = Entry(alarm_app, textvariable=hour, width=2, font=("Arial", 40), borderwidth=0)
+hour_input.place(x=300, y=100)
+
+minute = StringVar()
+minute.set('00')
+minute_input = Entry(alarm_app, textvariable=minute, width=2, font=("Arial", 40), borderwidth=0)
+minute_input.place(x=380, y=100)
+
+btn = Button(alarm_app, text="save", width=4, height=1, font=("Arial", 30), borderwidth=0, bg="green", fg="white", command=alarming)
+btn.place(x=325, y=200)
+
+rings_in = Label(alarm_app)
+rings_in.place(x=260, y=300)
+
+alarm_app.mainloop()
